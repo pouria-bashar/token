080809f0d389af512885a3ea536f514e3900175d
Pouria <pouria.bashardoust@gmail.com>
2016-05-15 15:39:55 +1000

    Added properties to TokenDocument

--- src/main/java/com/pba/token/entities/TokenDocument.java
+++ src/main/java/com/pba/token/entities/TokenDocument.java
@@ -1,5 +1,45 @@
 package com.pba.token.entities;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "token")
 public class TokenDocument {
+
+	private String id;
+	private String expiryMinutes;
+	private String createDateTime;
+	private String expiryDateTime;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getExpiryMinutes() {
+		return expiryMinutes;
+	}
+
+	public void setExpiryMinutes(String expiryMinutes) {
+		this.expiryMinutes = expiryMinutes;
+	}
+
+	public String getCreateDateTime() {
+		return createDateTime;
+	}
+
+	public void setCreateDateTime(String createDateTime) {
+		this.createDateTime = createDateTime;
+	}
+
+	public String getExpiryDateTime() {
+		return expiryDateTime;
+	}
+
+	public void setExpiryDateTime(String expiryDateTime) {
+		this.expiryDateTime = expiryDateTime;
+	}
 
 }
